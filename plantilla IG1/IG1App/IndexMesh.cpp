8389820ac4bc953bdc4876074f99de010d1c2365
javi <javari02@ucm.es>
2019-05-10 00:06:07 +0200

    Todo funciona

--- plantilla IG1/IG1App/IndexMesh.cpp
+++ plantilla IG1/IG1App/IndexMesh.cpp
@@ -19,36 +19,36 @@ void IndexMesh::render()
 	if (vertices != nullptr) {
 		glEnableClientState(GL_VERTEX_ARRAY);
 		glVertexPointer(3, GL_DOUBLE, 0, vertices);  // number of coordinates per vertex, type of each coordinate, stride, pointer 
+
+		if (colors != nullptr) {
+			glEnableClientState(GL_COLOR_ARRAY);
+			glColorPointer(4, GL_DOUBLE, 0, colors);   // number of coordinates per color, type of each coordinate, stride, pointer 
+		}
+
+		if (texCoords != nullptr) {
+			glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+			glTexCoordPointer(2, GL_DOUBLE, 0, texCoords);
+		}
+
+		if (normals != nullptr) {
+			glEnableClientState(GL_NORMAL_ARRAY);
+			glNormalPointer(GL_DOUBLE, 0, normals);
+		}
+
+		if (indices != nullptr) {
+			glEnableClientState(GL_INDEX_ARRAY);
+			glIndexPointer(GL_UNSIGNED_INT, 0, indices);
+		}
+
+		glDrawElements(primitive, numIndices, GL_UNSIGNED_INT, indices);
+
+
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+		glDisableClientState(GL_COLOR_ARRAY);
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_NORMAL_ARRAY);
+		glDisableClientState(GL_INDEX_ARRAY);
 	}
-
-	if (colors != nullptr) {
-		glEnableClientState(GL_COLOR_ARRAY);
-		glColorPointer(4, GL_DOUBLE, 0, colors);   // number of coordinates per color, type of each coordinate, stride, pointer 
-	}
-
-	if (texCoords != nullptr) {
-		glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-		glTexCoordPointer(2, GL_DOUBLE, 0, texCoords);
-	}
-
-	if (normals != nullptr) {
-		glEnableClientState(GL_NORMAL_ARRAY);
-		glNormalPointer(GL_DOUBLE, 0, normals);
-	}
-
-	if (indices != nullptr) {
-		glEnableClientState(GL_INDEX_ARRAY);  
-		glIndexPointer(GL_UNSIGNED_INT, 0, indices);
-	}
-
-	glDrawElements(primitive, 0, numVertices, indices);
-
-
-	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
-	glDisableClientState(GL_COLOR_ARRAY);
-	glDisableClientState(GL_VERTEX_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-	glDisableClientState(GL_INDEX_ARRAY);
 }
 
 IndexMesh * IndexMesh::generateGrid(GLdouble lado, GLuint numDiv)
